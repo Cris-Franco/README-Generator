3754a8daaf2532bd29fb7c95a38a26bae4fa6578
Cris Franco <cris.franco219@gmail.com>
2021-01-11 15:23:45 -0500

    created main 3 methods

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//Initialize requirements
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+
+//Function containing questions
+const promptUser = () => {
+
+};
+
+//Function to generate Markdown
+const generateMarkdown = (response) => {
+
+};
+
+//Function that initializes the program
+const init = async () => {
+
+};
